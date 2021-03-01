7ddc04d0a4153f354060015265523ad3f6d3155b
respinha <rui.ribeiro@sparetech.io>
2021-03-01 22:53:03 +0100

    Move navbar button to the corner

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -4,25 +4,25 @@ export class Navigation extends Component {
   render() {
     return (
       <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              {" "}
-              <span className="sr-only">Toggle navigation</span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-            </button>
-            <a className="navbar-brand page-scroll" href="#page-top">
-              Conangla
-            </a>{" "}
-          </div>
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Andrea Conangla
+          </a>{" "}
+        </div>
 
+        <div className="container">
           <div
             className="collapse navbar-collapse"
             id="bs-example-navbar-collapse-1"
