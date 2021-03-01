8b9827e24d7194f87ea24c42f3f190c57066c1f8
respinha <rui.ribeiro@sparetech.io>
2021-03-01 22:42:27 +0100

    Remove header subtitle

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,18 +6,20 @@ export class Header extends Component {
       <header id="header">
         <div className="intro">
           <div className="overlay">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8 col-md-offset-2 intro-text">
-                  <h1>
-                    {this.props.data ? this.props.data.title : "Loading"}
-                    <span></span>
-                  </h1>
-                  <p>
-                    {this.props.data ? this.props.data.paragraph : "Loading"}
-                  </p>
-                </div>
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {this.props.data ? this.props.data.title : "Loading"}
+                  <span></span>
+                </h1>
               </div>
+            </div>
+          </div>
+          <div className="overlay">
+            <div className="row">
+            <div className="col-md-8 col-md-offset-2" style={{ textAlign: 'center'}}>
+              <p>{" "}</p>
+            </div>
             </div>
           </div>
         </div>
