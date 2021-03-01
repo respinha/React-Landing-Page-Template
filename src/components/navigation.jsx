8a792400035aa044e1d0910365f8c9c47dabec2c
respinha <rui.ribeiro@sparetech.io>
2021-03-01 23:08:22 +0100

    Make navigation labels dynamic and bold

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
 
+const LABELS = [
+  "About",
+  "Features",
+  "Services",
+  "Gallery",
+  "Testimonials",
+  "Teams",
+  "Contact",
+];
 export class Navigation extends Component {
   render() {
     return (
@@ -28,41 +37,9 @@ export class Navigation extends Component {
             id="bs-example-navbar-collapse-1"
           >
             <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#features" className="page-scroll">
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="#about" className="page-scroll">
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#services" className="page-scroll">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" className="page-scroll">
-                  Gallery
-                </a>
-              </li>
-              <li>
-                <a href="#testimonials" className="page-scroll">
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a href="#team" className="page-scroll">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="page-scroll">
-                  Contact
-                </a>
-              </li>
+              {LABELS.map((label) => (
+                <NavbarItem label={label} />
+              ))}
             </ul>
           </div>
         </div>
@@ -70,5 +47,19 @@ export class Navigation extends Component {
     );
   }
 }
+
+const NavbarItem = ({ label }) => {
+  return (
+    <li>
+      <a
+        href={`#${label.toLowerCase()}`}
+        className="page-scroll"
+        style={{ fontWeight: "bold" }}
+      >
+        {label}
+      </a>
+    </li>
+  );
+};
 
 export default Navigation;
