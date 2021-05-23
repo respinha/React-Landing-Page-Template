7fa6291ca355373816c1fdb86fedc8804792b6c2
respinha <rui.ribeiro@sparetech.io>
2021-05-23 23:58:08 +0200

    Add tmp gallery

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
+const items = [8,10,3];
 export class Gallery extends Component {
   render() {
     return (
-      <div id="social-media" className="text-center">
+      <div id="gallery" className="text-center">
         <div className="container">
           <div className="section-title">
             <h2>Gallery</h2>
@@ -14,27 +15,17 @@ export class Gallery extends Component {
           </div>
           <div className="row">
             <div className="portfolio-items">
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/01-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/01-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
+              {items.map((item) => (
+                <div className="col-sm-6 col-md-4 col-lg-4">
+                  <div className="portfolio-item">
+                    <img
+                      src={`img/portfolio/${item}.jpeg`}
+                      className="img-responsive"
+                    />{" "}
                   </div>
                 </div>
-              </div>
+              ))}
+              {/* 
               <div className="col-sm-6 col-md-4 col-lg-4">
                 <div className="portfolio-item">
                   <div className="hover-bg">
@@ -202,7 +193,7 @@ export class Gallery extends Component {
                     </a>{" "}
                   </div>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
